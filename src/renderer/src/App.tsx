29dc51b4527cc65bab867a10def51cfa644d95ca
samirahmad5445 <samirahmad5445@gmail.com>
2024-11-14 00:51:09 +0200

    add: shadcn/ui library

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,9 +1,10 @@
+import { Button } from "./components/ui/button";
+
 function App(): JSX.Element {
   return (
-    <div className="grid font-mono bg-slate-950 place-content-center min-h-screen">
-      <h1 className="text-4xl justify-center items-center flex gap-1 text-orange-100 font-bold text-center">
-        <span>Wren</span>
-        <span className="block p-2 bg-sky-600 rounded-xl">Studio</span>
+    <div className="grid min-h-screen place-content-center font-mono">
+      <h1 className="flex items-center justify-center gap-1 text-center text-4xl font-bold text-orange-100">
+        <Button variant="link">Click Me!</Button>
       </h1>
     </div>
   );
