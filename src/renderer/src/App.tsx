75965bb0e013f9c284f70e03e4660b52ae51cdfc
samirahmad5445 <samirahmad5445@gmail.com>
2024-11-19 00:11:59 +0200

    add: custom theme

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -2,9 +2,9 @@ import { Button } from "./components/ui/button";
 
 function App(): JSX.Element {
   return (
-    <div className="grid min-h-screen place-content-center font-mono">
+    <div className="grid min-h-screen place-content-center">
       <h1 className="flex items-center justify-center gap-1 text-center text-4xl font-bold text-orange-100">
-        <Button variant="link">Click Me!</Button>
+        <Button variant="default">Click Me!</Button>
       </h1>
     </div>
   );
